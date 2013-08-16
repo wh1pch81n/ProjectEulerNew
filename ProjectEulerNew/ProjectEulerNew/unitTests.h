260de3fdad685329434d012a7e6a034cf3ac7184
wh1pch81n <wh1pch81n@gmail.com>
2013-08-16 13:14:48 -0700

    dh-2 adding sieve class and unit testing

---
+++ ProjectEulerNew/ProjectEulerNew/unitTests.h
@@ -0,0 +1,14 @@
+//
+//  unitTests.h
+//  ProjectEulerNew
+//
+//  Created by Derrick Ho on 8/16/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HeaderAPI.h"
+
+@interface unitTests : NSObject
++ (void)listOfPrimesUnder100;
+@end
