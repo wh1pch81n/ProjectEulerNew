972c11e963aa73f11035eace6557b43f44ab8bef
wh1pch81n <wh1pch81n@gmail.com>
2013-09-09 18:07:48 -0700

    Modified dhhalfadderbase10 for more simplicity.

--- ProjectEulerNew/ProjectEulerNew/DHHalfAdderBase10.m
+++ ProjectEulerNew/ProjectEulerNew/DHHalfAdderBase10.m
@@ -7,55 +7,24 @@
 //
 
 #import "DHHalfAdderBase10.h"
+const NSInteger kBase = 10;
+
 @implementation DHHalfAdderBase10
-- (id)initWithNumA:(NSNumber *)numa withNumB:(NSNumber *)numb {
-	if (!(self = [super init]) || !(_numa = [self checkBoundsOfNum:numa]) || !(_numb = [self checkBoundsOfNum:numb])) {
-		return nil;
+@synthesize numa = _numa;
+@synthesize numb = _numb;
+- (id)initWithNumA:(NSInteger)numa withNumB:(NSInteger)numb {
+	if (self = [super init]) {
+		[self setNuma:numa%kBase];
+		[self setNumb:numb%kBase];
 	}
-	//init code
 	return self;
 }
-- (id)initWithNumA:(NSNumber *)numa {
-	return [self initWithNumA:numa withNumB:@(0)];
-}
-- (id)init {
-	return [self initWithNumA:@(0) withNumB:@(0)];
-}
-- (NSNumber *)checkBoundsOfNum:(NSNumber *)num {
-	@try {
-		if (!num) {
-			NSException *e = [NSException exceptionWithName:kExceptionNilNum
-													 reason:kExceptionNilNumReason
-												   userInfo:nil];
-			@throw e;
-		}else if ([num integerValue] <0 || [num integerValue] > 9) {
-			NSException *e = [NSException exceptionWithName:kExceptionNumOutOfBounds
-													 reason:kExceptionNumOutOfBoundsReason
-												   userInfo:nil];
-			@throw e;
-		}
-	} @catch (NSException *theException) {
-		if ([[theException name] isEqualToString:kExceptionNilNum] ||
-			[[theException name] isEqualToString:kExceptionNumOutOfBounds]) {
-			
-			NSLog(@"caught a %@ (%@)", [theException name], num);
-		} else {
-			NSLog(@"ignored a %@ exception", theException);
-			@throw;
-		}
-		num = nil;
-	}
-	return num;
-}
-#pragma mark sum of inputs
-- (NSInteger)sumOfInputs {
-	return [_numa integerValue] + [_numb integerValue];
-}
+
 #pragma mark - outputs
-- (NSNumber *)solution {
-	return @([self sumOfInputs] % kBase);
+- (NSInteger)solution {
+	return ([self numa] + [self numb])% kBase;
 }
-- (NSNumber *)cout {
-	return @([self sumOfInputs] / kBase);
+- (NSInteger)cout {
+	return ([self numa] + [self numb]) / kBase;
 }
 @end
