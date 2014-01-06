17647038be5b4b6e58a5c3769a36ee2ca738c2e8
wh1pch81n <wh1pch81n@gmail.com>
2014-01-05 22:47:22 -0800

    Changing the way I call the Unittests

--- ProjectEulerNew/ProjectEulerNew/main.m
+++ ProjectEulerNew/ProjectEulerNew/main.m
@@ -13,46 +13,34 @@ int main(int argc, const char * argv[])
 {
 	
 	@autoreleasepool {
+		NSArray *arrofTests =
+		@[
+			[NSValue valueWithPointer:@selector(listOfPrimesUnder100)],
+			[NSValue valueWithPointer:@selector(prob16_0_halfAdder)],
+			[NSValue valueWithPointer:@selector(prob16_1_fullAdder)],
+			[NSValue valueWithPointer:@selector(prob16_2_bigInteger)],
+			[NSValue valueWithPointer:@selector(prob16_3_sumOfDigits)],
+			[NSValue valueWithPointer:@selector(prob16_4_multDigits)],
+			[NSValue valueWithPointer:@selector(prob17_0_wordFormOfNumbers)],
+			[NSValue valueWithPointer:@selector(prob17_1_numFrom1to1000)],
+			/**Triangles throwing exception: beyond bounds*/
+			//[NSValue valueWithPointer:@selector(prob18_0_smallTriangle)],
+			//[NSValue valueWithPointer:@selector(prob18_1_medTriangle)],
+			[NSValue valueWithPointer:@selector(prob20_0_FactorialDigitSum)],
+			[NSValue valueWithPointer:@selector(prob21_0_isAmicableNumber)],
+			[NSValue valueWithPointer:@selector(prob21_1_sumOfAllAmicableUnder10000)],
+			[NSValue valueWithPointer:@selector(prob22_0_NameScore)]
+			];
 		
-		//[unitTests listOfPrimesUnder100];
-		//[unitTests prob16_0_halfAdder];
+		unitTests *UT = [unitTests new];
+		for (int i = 0; i < arrofTests.count; ++i) {
+			printf("\n======================\n");
+			[UT performSelector:[arrofTests[i] pointerValue]];
+		}
 		
-		//printf("======================\n");
-		//[unitTests prob16_1_fullAdder];
 		
-		//printf("======================\n");
-		//[unitTests prob16_2_bigInteger];
 		
-		//printf("======================\n");
-		//[unitTests prob16_3_sumOfDigits];
-		
-		//printf("======================\n");
-		//[unitTests prob16_4_multDigits];
-		
-		//printf("======================\n");
-		//[unitTests prob17_0_wordFormOfNumbers];
-		
-		//printf("======================\n");
-		//[unitTests prob17_1_numFrom1to1000];
-		
-		//printf("======================\n");
-		//[unitTests prob18_0_smallTriangle];
-		
-		//printf("======================\n");
-		//[unitTests prob18_1_medTriangle];
-		
-		//printf("======================\n");
-		//[unitTests prob20_0_FactorialDigitSum];
-		
-		//printf("======================\n");
-		//[unitTests prob21_0_isAmicableNumber];
-		
-		//printf("======================\n");
-		//[unitTests prob21_1_sumOfAllAmicableUnder10000];
-		
-		printf("======================\n");
-		[unitTests prob22_0_NameScore];
 	}
-    return 0;
+	return 0;
 }
 
